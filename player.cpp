b7095eea39d6390a80dc2af538cb05c17dab73ce
laurenli06 <li.lauren42@gmail.com>
2018-03-05 21:03:49 -0800

    did something

--- player.cpp
+++ player.cpp
@@ -11,12 +11,15 @@
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
     testingMinimax = false;
+    Board mmmboard = Board();
+    
+    
 
     /*
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
      * 30 seconds.
-     */
+     */ 
 }
 
 /*
